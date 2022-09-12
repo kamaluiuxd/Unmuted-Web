3a5c5abfc3afd9d5f677b8bb9fc825a7bfdda375
kamal <kamaluiuxd@gmail.com>
2022-09-12 19:58:23 +0530

    Nav Menu updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import Header from "./component/Header/Header";
+import { Routes, Route } from "react-router-dom";
+import Home from "./component/Pages/Home";
+import About from "./component/Pages/About";
+import Portfolio from "./component/Pages/Portfolio";
+import Projects from "./component/Pages/Projects";
+import Contact from "./component/Pages/Contact";
 
 function App() {
-	return <Header />;
+	return (
+		<section>
+			<Header />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="about" element={<About />} />
+				<Route path="portfolio" element={<Portfolio />} />
+				<Route path="projects" element={<Projects />} />
+				<Route path="contact" element={<Contact />} />
+			</Routes>
+		</section>
+	);
 }
 
 export default App;
--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,44 +1,51 @@
-import { FaFacebookSquare, FaTwitterSquare, FaLinkedin } from "react-icons/fa";
-import logo from "../../assets/home/logo.png";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+//import { FaFacebookSquare, FaTwitterSquare, FaLinkedin, FaBars } from "react-icons/fa";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+//import logo from "../../assets/home/logo.png";
+
+///------------------------------------------------------------------------///
 const Header = () => {
-	const NavLinks = [
-		{ id: 1, link: "Home" },
-		{ id: 2, link: "About" },
-		{ id: 3, link: "Portfolio" },
-		{ id: 4, link: "Projects" },
-		{ id: 5, link: "Contact" },
-	];
+	const [nav, setNav] = useState(true);
+
 	return (
 		<section className=" container-fluid  bg-slate-800">
-			<section className="py-2 flex items-center text-white justify-around">
+			<section className="py-5 flex items-center text-white justify-between">
 				<div>
-					<img src={logo} alt="unmuted-home" />
+					<h1 className="w-full text-2xl font-bold ml-10">
+						UNMUTED <span className="text-red-400">WEB.</span>
+					</h1>
 				</div>
-				<ul className="hidden md:flex items-center">
-					{NavLinks.map(({ id, link }) => (
-						<li key={id} className="mr-10 cursor-pointer">
-							{link}
-						</li>
-					))}
-					<FaFacebookSquare
-						size={40}
-						className="cursor-pointer px-2 text-white-100 hover:scale-150 duration-200"
-					/>
-					<FaTwitterSquare
-						size={40}
-						className="cursor-pointer px-2 text-white-100 hover:scale-150 duration-200"
-					/>
-					<FaLinkedin
-						size={40}
-						className="cursor-pointer px-2 text-white-100 hover:scale-150 duration-200"
-					/>
+				<ul className="hidden md:flex items-center space-x-3">
+					<Link to="/">Home</Link>
+					<Link to="about">About</Link>
+					<Link to="portfolio">Portfolio</Link>
+					<Link to="projects">Projects</Link>
+					<Link to="contact">Contact</Link>
 					<button
-						className="mx-10 px-5 py-3 rounded cursor-pointer
+						className="mr-10 px-5 py-3 rounded cursor-pointer
                     bg-gradient-to-br from-cyan-500 to-blue-400 hover:scale-105 duration-300"
 					>
-						Enquiry Now
+						Let's Talk
 					</button>
 				</ul>
+
+				<div className="pr-5 block md:hidden" onClick={() => setNav(!nav)}>
+					{!nav ? <AiOutlineClose size={25} /> : <AiOutlineMenu size={25} />}
+				</div>
+
+				<div className={!nav ? "fixed left-0 top-0 bg-slate-900 w-[70%] h-full ease-in-out duration-500" : "fixed left-[-100%]"}>
+					<h1 className="w-full text-2xl font-bold p-4 ">
+						UNMUTED <span className="text-red-400">WEB.</span>
+					</h1>
+					<ul className="pt-5 flex flex-col space-y-10 m-5">
+						<Link to="/">Home</Link>
+						<Link to="about">About</Link>
+						<Link to="portfolio">Portfolio</Link>
+						<Link to="projects">Projects</Link>
+						<Link to="contact">Contact</Link>
+					</ul>
+				</div>
 			</section>
 		</section>
 	);
